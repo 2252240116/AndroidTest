d989a2e3860b4ae0c24a1c57c7b65ceb5c64ddf1
Riven.zhang <Riven.zhang@gaodun.com>
2023-10-31 18:09:04 +0800

    绘制流程

--- app/src/main/java/com/riven/test/eventdispatch/ViewTestCCC.java
+++ app/src/main/java/com/riven/test/eventdispatch/ViewTestCCC.java
@@ -2,6 +2,7 @@ package com.riven.test.eventdispatch;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.graphics.Canvas;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.MotionEvent;
@@ -47,5 +48,58 @@ public class ViewTestCCC extends View {
 
     public ViewTestCCC(Context context, @Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
         super(context, attrs, defStyleAttr, defStyleRes);
+    }
+
+    /**
+     * 这int两个字表示宽高和各自方向的测量模式合成的值
+     *
+     * @param widthMeasureSpec
+     * @param heightMeasureSpec
+     * UNSPECIFIED	00	默认值，父控件没有给子view任何限制，子View可以设置为任意大小。
+     * EXACTLY	01	表示父控件已经确切的指定了子View的大小。
+     * AT_MOST	10	表示子View具体大小没有尺寸限制，但是存在上限，上限一般为父View大小。
+     */
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        //如果改变了View宽高，不要调用这行代码。需要调用setMeasuredDimension(widthsize,heightsize);
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        int widthsize = MeasureSpec.getSize(widthMeasureSpec);      //取出宽度的确切数值
+        int widthmode = MeasureSpec.getMode(widthMeasureSpec);      //取出宽度的测量模式
+
+        int heightsize = MeasureSpec.getSize(heightMeasureSpec);    //取出高度的确切数值
+        int heightmode = MeasureSpec.getMode(heightMeasureSpec);    //取出高度的测量模式
+    }
+
+    /**
+     * 已经有onMeasured方法测试大小了，为啥还需要这个方法呢
+     * 因为view的大小不仅由自身控制，还受父控件影响
+     * @param w 最终大小
+     * @param h 最终大小
+     * @param oldw
+     * @param oldh
+     */
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+
+    }
+
+    /**
+     *
+     * @param changed
+     * @param left 左侧距离父View left距离
+     * @param top  顶部距离父View top的距离
+     * @param right 右侧距离父View左侧的距离
+     * @param bottom 底部距离父View顶部的距离
+     */
+    @Override
+    protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
+        super.onLayout(changed, left, top, right, bottom);
+
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
     }
 }
