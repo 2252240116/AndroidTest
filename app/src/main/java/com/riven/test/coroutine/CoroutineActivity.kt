d2f5310fddedba0bcc75fe1cb3e71b70c2cb549c
Riven.zhang <Riven.zhang@gaodun.com>
2023-12-13 14:42:50 +0800

    协程原理

--- app/src/main/java/com/riven/test/coroutine/CoroutineActivity.kt
+++ app/src/main/java/com/riven/test/coroutine/CoroutineActivity.kt
@@ -10,10 +10,14 @@ import kotlinx.android.synthetic.main.activity_coroutine.*
 import kotlinx.coroutines.*
 
 /**
- * Function:
+ * Function: 协程系列文章：https://juejin.cn/post/6962921891501703175
+ *                        https://juejin.cn/post/7113706345190129700
  * Author Name: Riven.zhang
  * Date: 2023/12/7
- * Copyright © 2006-2021 高顿网校, All Rights Reserved.
+ * 在Activity中使用协程建议使用封装好的lifecycle库，自己使用协程也可以，但是得解决几个问题
+ * 1.考虑协程在主线程中刷新ui
+ * 2.子协程异常会打扰父协程，父协程取消会影响子其他协程，导致无法运行
+ * 3.考虑生命周期的退出，这时协程还在运行，会导致crash或者泄漏
  */
 class CoroutineActivity :AppCompatActivity(){
     private val normalScope = NormalScope()
@@ -28,31 +32,32 @@ class CoroutineActivity :AppCompatActivity(){
     override fun onResume() {
         super.onResume()
         Log.d("init", "onResume")
-
     }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_coroutine)
-
+        //协程的启动
         tv_launch.setOnClickListener {
             start1()
         }
-
+        //协程的同步
         tv_launch1.setOnClickListener {
             startConcurrencyAndSynchronous()
         }
-
+        //协程的CoroutineContext
         tv_launch2.setOnClickListener {
             testCoroutineContext()
         }
-
+        //协程的启动模式
         tv_launch3.setOnClickListener {
             testCoroutineStart()
         }
-
+        //协程的作用域(主作用域与协同作用域）
         tv_launch4.setOnClickListener {
             testCoroutineScope3()
         }
+
         //并发
         requestMain {
             delay(2000)
@@ -71,6 +76,77 @@ class CoroutineActivity :AppCompatActivity(){
         var job : Job  = customCoroutineRequest{
 
         }
+        //协程原理
+        tv_coroutine_principle.setOnClickListener {
+            MainScope().launch {
+                testCor()
+            }
+        }
+    }
+
+    /**
+     *  suspend本质是callBack ，kotlin里叫Continuation(续体）
+     *  Continuation:接下来需要执行的代码or剩下来的代码
+     *  suspend方法反编译会形成一个 带形参continution 的方法
+     *
+     *  public interface Continuation<in T> {
+        public val context: CoroutineContext
+            //相当于 onSuccess     结果
+            public fun resumeWith(result: Result<T>)
+        }
+
+        suspend方法返回值是个Any，标志函数有没有被挂起。(因为可能返回是枚举、"no suspend"、null所以定义为Any)
+        testCor()方法内部会调用invokeSuspend()方法,invokeSuspend嵌套testCor()方法
+
+        内部有两个字段label(状态机，可理解为每一个supsend方法是一个case状态）result(协程返回值）
+        当执行一个伪supsend方法，会重新走一遍Switch切换到下一个状态机
+
+        他们公用同一个Continuation实例(轻量级）
+        协程完全由开发者管理，不涉及操作系统的调度和切换，因此高效
+     */
+    private suspend fun testCor() {
+        // 从主线程切到子线程（协程的挂起）从子线程切回主线程（协程的恢复resume)
+        // ↓主线程   ↓子线程
+        val user = getUserInfo()
+        val friendList = getFriendList(user)
+        val feedList = getFeedList(friendList)
+        feedList.forEach {
+            Log.d("打印", it)
+        }
+    }
+
+    //挂起函数
+    suspend fun getUserInfo(): String {
+        Log.d("打印","开始getUserInfo")
+        //挂起函数包含其他挂起函数才叫真正的挂起
+        withContext(Dispatchers.IO) {
+            delay(1000L)
+        }
+        Log.d("打印","返回BoyCoder")
+
+        return "BoyCoder"
+    }
+    //挂起函数
+    suspend fun getFriendList(user: String): String {
+        Log.d("打印","开始getFriendList")
+
+        withContext(Dispatchers.IO) {
+            delay(1000L)
+        }
+        Log.d("打印","返回Tom, Jack")
+
+        return "Tom, Jack"
+    }
+    //挂起函数
+    suspend fun getFeedList(list: String): ArrayList<String> {
+        Log.d("打印","开始getFeedList")
+
+        withContext(Dispatchers.IO) {
+            delay(1000L)
+        }
+        Log.d("打印","返回在线状态")
+
+        return arrayListOf<String>("在线","离线","繁忙","勿扰","Q我吧")
     }
 
     protected fun customCoroutineRequest(
